41cedc0ec92e788101e5be5922afaf537a8d3a60
Luis Gutierrez <luisdgutierrez05@gmail.com>
2019-06-20 21:01:12 -0500

    Fix SyntaxError on queue subscription file

--- lib/propono/components/queue_subscription.rb
+++ lib/propono/components/queue_subscription.rb
@@ -26,19 +26,19 @@ module Propono
     end
 
     def create_and_subscribe_main_queue
-      @queue = aws_client.create_queue({queue_name: "#{queue_name}.fifo, attributes: {FifoQueue: true})
+      @queue = aws_client.create_queue(queue_name: "#{queue_name}.fifo", attributes: { FifoQueue: true })
       topic = aws_client.create_topic(@suffixed_topic_name)
       aws_client.subscribe_sqs_to_sns(@queue, topic)
       aws_client.set_sqs_policy(@queue, generate_policy(@queue, topic))
     end
 
     def create_misc_queues
-      @failed_queue = aws_client.create_queue({queue_name: "#{queue_name}-failed.fifo", attributes: {FifoQueue: true})
-      @corrupt_queue = aws_client.create_queue({queue_name: "#{queue_name}-corrupt.fifo", attributes: {FifoQueue: true})
+      @failed_queue = aws_client.create_queue(queue_name: "#{queue_name}-failed.fifo", attributes: { FifoQueue: true })
+      @corrupt_queue = aws_client.create_queue(queue_name: "#{queue_name}-corrupt.fifo", attributes: { FifoQueue: true })
     end
 
     def create_and_subscribe_slow_queue
-      @slow_queue = aws_client.create_queue({queue_name: "#{queue_name}-slow.fifo", attributes: {FifoQueue: true})
+      @slow_queue = aws_client.create_queue(queue_name: "#{queue_name}-slow.fifo", attributes: { FifoQueue: true })
       slow_topic = aws_client.create_topic(@suffixed_slow_topic_name)
       aws_client.subscribe_sqs_to_sns(@slow_queue, slow_topic)
       aws_client.set_sqs_policy(@slow_queue, generate_policy(@slow_queue, slow_topic))
@@ -48,26 +48,26 @@ module Propono
 
     def generate_policy(queue, topic)
       <<-EOS
-{
-  "Version": "2008-10-17",
-  "Id": "#{queue.arn}/SQSDefaultPolicy",
-  "Statement": [
-    {
-      "Sid": "#{queue.arn}-Sid",
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "*"
-      },
-      "Action": "SQS:*",
-      "Resource": "#{queue.arn}",
-      "Condition": {
-        "StringEquals": {
-          "aws:SourceArn": "#{topic.arn}"
+        {
+          "Version": "2008-10-17",
+          "Id": "#{queue.arn}/SQSDefaultPolicy",
+          "Statement": [
+            {
+              "Sid": "#{queue.arn}-Sid",
+              "Effect": "Allow",
+              "Principal": {
+                "AWS": "*"
+              },
+              "Action": "SQS:*",
+              "Resource": "#{queue.arn}",
+              "Condition": {
+                "StringEquals": {
+                  "aws:SourceArn": "#{topic.arn}"
+                }
+              }
+            }
+          ]
         }
-      }
-    }
-  ]
-}
       EOS
     end
   end
